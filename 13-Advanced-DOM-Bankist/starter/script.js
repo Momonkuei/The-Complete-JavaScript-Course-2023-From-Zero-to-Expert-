891570d01b152af8d7ce3ca726a0524b48bf4553
Jackson Chen <momonbin1991@icloud.com>
2023-04-11 07:12:52 +0800

    189. Types of Events and Event Handlers事件類型和事件處理程序完成

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -67,3 +67,23 @@ btnScrollTo.addEventListener('click', function (e) {
   // 第二種方法
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener : Great ! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.addEventListener('mouseenter', function (e) {
+//   alert('addEventListener : Great ! You are reading the heading :D');
+// });
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener : Great ! You are reading the heading :D');
+// };
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1));
